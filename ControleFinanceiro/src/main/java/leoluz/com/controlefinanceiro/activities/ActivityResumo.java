90fd1bcd7df9ad597161a8774ff415adaccb795a
leandroluz <engleoluz@hotmail.com>
2019-01-03 21:47:40 -0200

    #9 - Implementação da tela de resumo

--- ControleFinanceiro/src/main/java/leoluz/com/controlefinanceiro/activities/ActivityResumo.java
+++ ControleFinanceiro/src/main/java/leoluz/com/controlefinanceiro/activities/ActivityResumo.java
@@ -8,57 +8,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
+import adapter.BancoAdapterCompleto;
+import adapter.CategoriaAdapterCompleto;
+import adapter.ContaAdapterCompleto;
+import adapter.ElementoAdapterCompleto;
+import adapter.ItemAdapterCompleto;
+import adapter.SubItemAdapterCompleto;
+import adapter.TipoAdapterCompleto;
 import leoluz.com.controlefinanceiro.R;
+import model.Banco;
+import model.Carteira;
+import model.Categoria;
+import model.Conta;
+import model.Elemento;
+import model.Item;
+import model.Lancamento;
+import model.SubItem;
+import model.Tipo;
+import persist.DAO.ContaDao;
+import persist.DAO.FabricaDao;
 
-public class ActivityResumo extends AppCompatActivity implements View.OnClickListener {
-    private Button botao;
+public class ActivityResumo extends AppCompatActivity  {
 
-    static final int DATE_DIALOG_ID = 0;
+    String opcao;
+    List<Conta> listaConta;
+    ContaAdapterCompleto adapterConta;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.resumo);
+        listView = new ListView(this);
+        setContentView(listView);
 
-        botao = (Button) findViewById(R.id.btn);
-        botao.setOnClickListener(this);
-    }
+        //alterando o titulo da tela
+        setTitle("SALDO DAS CONTAS");
 
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        Calendar calendario = Calendar.getInstance();
+        TextView txtHeader = new TextView(this);
+        txtHeader.setText(R.string.txt_cabecalho_listaResumo);
+        listView.addHeaderView(txtHeader);
 
-        int ano = calendario.get(Calendar.YEAR);
-        int mes = calendario.get(Calendar.MONTH);
-        int dia = calendario.get(Calendar.DAY_OF_MONTH);
-
-        switch (id) {
-            case DATE_DIALOG_ID:
-                return new DatePickerDialog(this, mDateSetListener, ano, mes,
-                        dia);
+        try {
+            ContaDao contaDao = FabricaDao.criarContaDao();
+            listaConta = new ArrayList<>();
+            listaConta = contaDao.listarTodos();
+            adapterConta = new ContaAdapterCompleto(this, listaConta);
+            listView.setAdapter(adapterConta);
+        } catch (Exception e) {
+            Toast.makeText(this, "Erro: ao consultar a lista", Toast.LENGTH_SHORT).show();
+            sair();
         }
-        return null;
     }
 
-    private DatePickerDialog.OnDateSetListener mDateSetListener =
-            new DatePickerDialog.OnDateSetListener() {
-                public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                      int dayOfMonth) {
-                    String data = String.valueOf(dayOfMonth) + " /"
-                            + String.valueOf(monthOfYear + 1) + " /" + String.valueOf(year);
-                    Toast.makeText(ActivityResumo.this,
-                            "DATA = " + data, Toast.LENGTH_SHORT)
-                            .show();
-                }
-            };
 
-    @Override
-    public void onClick(View v) {
-        if (v == botao)
-            showDialog(DATE_DIALOG_ID);
+    private void sair() {
+        Intent i = new Intent(this, ActivityMenu.class);
+        startActivity(i);
+        finish();
     }
+
+
 }
